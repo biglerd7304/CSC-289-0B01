b439ce808e78e2db5404ce3e555062781539d3a4
biglerd7304 <43176266+biglerd7304@users.noreply.github.com>
2020-03-15 07:40:14 -0400

    completed more

--- FlaskQuickStart/Quick.py
+++ FlaskQuickStart/Quick.py
@@ -6,6 +6,8 @@ Created on Thu Mar 12 21:57:06 2020
 @author: daniel
 """
 from flask import Flask
+from markupsafe import escape
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -15,3 +17,8 @@ def index ():
 @app.route('/hello')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return 'User %s'  % escape(username)
