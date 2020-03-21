5870bee11f768d2a708b711eab47c408d0480cd8
biglerd7304 <43176266+biglerd7304@users.noreply.github.com>
2020-03-21 00:04:37 -0400

    Update Quick.py

--- FlaskQuickStart/Quick.py
+++ FlaskQuickStart/Quick.py
@@ -5,12 +5,12 @@ Created on Thu Mar 12 21:57:06 2020
 
 @author: daniel
 """
-from flask import Flask
+from flask import Flask, url_for
 from markupsafe import escape
 
 app = Flask(__name__)
 
-
+'''
 #ROUTING
 @app.route('/')
 def index ():
@@ -37,3 +37,29 @@ def show_subpath(subpath):
     return 'Subpath %s' % escape(subpath)
 
 #UNIQUE URL/REDIRECTION BEHAVIOR
+@app.route('/projects/')
+def projects():
+    return 'The project page'
+
+@app.route('/about')
+def about():
+    return 'The about page'
+'''
+#URL BUILDING
+@app.route('/')
+def index():
+    return 'index'
+
+@app.route('/login')
+def login():
+    return 'login'
+
+@app.route('/user/<username>')
+def profile(username):
+    return '{}\'s profile'.format(escape(username))
+
+with app.test_request_context():
+    print(url_for('index'))
+    print(url_for('login'))
+    print(url_for('login', next='/'))
+    print(url_for('profile', username='John Doe'))
