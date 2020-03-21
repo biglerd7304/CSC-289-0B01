7d706477861ac5caaef359a2352237cdf68c87a8
biglerd7304 <43176266+biglerd7304@users.noreply.github.com>
2020-03-20 23:56:24 -0400

    Update Quick.py

--- FlaskQuickStart/Quick.py
+++ FlaskQuickStart/Quick.py
@@ -10,6 +10,8 @@ from markupsafe import escape
 
 app = Flask(__name__)
 
+
+#ROUTING
 @app.route('/')
 def index ():
     return 'Index Page'
@@ -18,7 +20,20 @@ def index ():
 def hello_world():
     return 'Hello, World!'
 
+#VARIABLE RULES
 @app.route('/user/<username>')
 def show_user_profile(username):
     # show the user profile for that user
     return 'User %s'  % escape(username)
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return 'Post %d' % post_id
+
+@app.route('/path/<path:subpath>')
+def show_subpath(subpath):
+    # show the subpath after /path/
+    return 'Subpath %s' % escape(subpath)
+
+#UNIQUE URL/REDIRECTION BEHAVIOR
